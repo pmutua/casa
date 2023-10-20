3f0b990a5727f1adc3dd484b8ef23a95a7e439c0
pmutua <pmutua@live.com>
2023-10-20 16:53:11 +0300

    feat: database configurations and database script implementation

---
+++ db_seed.py
@@ -0,0 +1,45 @@
+import sqlite3
+import json
+
+try:
+    # Establish connection to the SQLite database
+    conn = sqlite3.connect('property_listings.db')
+    cursor = conn.cursor()
+
+    # Read property data from JSON file
+    with open('default_property_listings.json', 'r') as file:
+        property_data = json.load(file)
+
+    # Create a property table if it doesn't exist
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS properties (
+                       id INTEGER PRIMARY KEY,
+                       location TEXT,
+                       bedrooms INTEGER,
+                       bathrooms INTEGER,
+                       price REAL
+                   )
+    """)
+
+    # Insert the property data into the database
+    for prop in property_data:
+        location = prop['location']
+        bedrooms = prop['bedrooms']
+        bathrooms = prop['bathrooms']
+        price = prop['price']
+
+        cursor.execute("INSERT INTO properties (location, bedrooms, bathrooms, price) VALUES (?,?,?,?)", (location, bedrooms, bathrooms, price))
+
+    # Commit the changes
+    conn.commit()
+
+except sqlite3.Error as e:
+    print("SQLite error:", e)
+
+except json.JSONDecodeError as e:
+    print("JSON decode error:", e)
+
+finally:
+    # Close the connection in a finally block
+    if conn:
+        conn.close()
